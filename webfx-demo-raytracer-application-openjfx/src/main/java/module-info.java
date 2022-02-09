1bd94aec244287c5a10c325ca0193e6db5752511
Bruno Salmon <dev.salmonb@gmail.com>
2022-02-09 13:59:10 +0000

    Changed module token from "javafx" to "openjfx" when designating OpenJFX runtime

--- webfx-demo-raytracer-application-openjfx/src/main/java/module-info.java
+++ webfx-demo-raytracer-application-openjfx/src/main/java/module-info.java
@@ -1,10 +1,10 @@
 // File managed by WebFX (DO NOT EDIT MANUALLY)
 
-module webfx.demo.raytracer.application.javafx {
+module webfx.demo.raytracer.application.openjfx {
 
     // Direct dependencies modules
     requires webfx.demo.raytracer.application;
-    requires webfx.kit.javafx;
+    requires webfx.kit.openjfx;
     requires webfx.platform.java.appcontainer.impl;
     requires webfx.platform.java.scheduler.impl;
     requires webfx.platform.java.shutdown.impl;
