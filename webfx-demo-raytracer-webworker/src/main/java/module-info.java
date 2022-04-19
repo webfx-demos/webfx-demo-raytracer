a236ea63d51873e98c8e2b1ddbdf75f847dfdaa6
Bruno Salmon <dev.salmonb@gmail.com>
2022-04-19 12:31:19 +0100

    Moved async API to a separate module (webfx-platform-shared-async)

--- webfx-demo-raytracer-webworker/src/main/java/module-info.java
+++ webfx-demo-raytracer-webworker/src/main/java/module-info.java
@@ -5,6 +5,7 @@ module webfx.demo.raytracer.webworker {
     // Direct dependencies modules
     requires webfx.demo.raytracer.math;
     requires webfx.platform.client.webassembly;
+    requires webfx.platform.shared.async;
     requires webfx.platform.shared.json;
     requires webfx.platform.shared.util;
     requires webfx.platform.shared.webworker;
