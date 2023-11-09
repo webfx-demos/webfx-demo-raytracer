7e222701fa6f45ce3f9c3b5d0b11f9e1d008f3e5
Bruno Salmon <dev.salmonb@gmail.com>
2023-11-09 09:10:47 +0000

    Ran 'webfx update'

--- webfx-demo-raytracer-application-gluon/src/main/java/module-info.java
+++ webfx-demo-raytracer-application-gluon/src/main/java/module-info.java
@@ -16,7 +16,4 @@ module webfx.demo.raytracer.application.gluon {
     requires webfx.platform.useragent.gluon;
     requires webfx.platform.webworker.java;
 
-    // Resources packages
-    opens dev.webfx.platform.meta.exe;
-
 }
